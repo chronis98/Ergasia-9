cea40bded57ed2d1d4ce6ebb7a35dbd6b4c4f36b
chronis98 <xmd@protonmail.com>
2017-02-26 01:32:48 +0200

    Add files via upload

---
+++ ergasia 9.py
@@ -0,0 +1,21 @@
+import json
+import urllib2
+max=-10**3
+name=""
+url=urllib2.urlopen("https://api.brewerydb.com/v2/search?q=Belgian%20And%20French%20Origin%20Ales&type=beer&key=fec0b9bd7c6ea4f30145da0a544aed12&format=json")
+apotelesma=json.load(url)
+lekseis=raw_input("dwse lekseis xwrismenes me komma")
+lekseis=lekseis.split(",")
+for i in range(len(lekseis)):
+    for k in apotelesma['data']:
+        sum=0
+        if 'description' in k:
+         if lekseis[i] in k['description']:
+            sum+=k['description'].count(lekseis[i])
+         if 'description' in k['style']:
+            if lekseis[i] in k['style']['description']:
+             sum+=k['style']['description'].count(lekseis[i])
+        if sum>max:
+          max=sum
+          name=k['name']
+print "Symfwna me tis lekseis pou dwsate h katallhlh byra gia esas einai h :",name
