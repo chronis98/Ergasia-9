1ef1559ad4956323c6139f598575813942689def
chronis98 <xmd@protonmail.com>
2017-02-26 19:55:43 +0200

    Update ergasia 9.py

--- ergasia 9.py
+++ ergasia 9.py
@@ -2,6 +2,7 @@ import json
 import urllib2
 max=-10**3
 name=""
+#xrhsimpoihsa thn methodo search kai anazhthsa ena sygkekrimeno style (Belgian And French Origin Ales) gia na mou dwsei polla apotelesmata
 url=urllib2.urlopen("https://api.brewerydb.com/v2/search?q=Belgian%20And%20French%20Origin%20Ales&type=beer&key=fec0b9bd7c6ea4f30145da0a544aed12&format=json")
 apotelesma=json.load(url)
 lekseis=raw_input("dwse lekseis xwrismenes me komma")
